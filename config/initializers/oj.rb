6da03aed72a3494a6b462046e5d0bcd3ef0058f1
Michal Trzcinka <michal.trzcinka@allani.pl>
2018-02-05 12:42:46 +0100

    Trying bigdecimal_as_decimal fix

--- config/initializers/oj.rb
+++ config/initializers/oj.rb
@@ -1,2 +1,6 @@
-Oj.optimize_rails
-Oj.default_options = { mode: :compat, bigdecimal_as_decimal: true }
+#Oj.default_options = { bigdecimal_as_decimal: true }
+#Oj.optimize_rails
+
+Oj::Rails.set_encoder()
+Oj::Rails.optimize()
+Oj.default_options = { bigdecimal_as_decimal: true }
